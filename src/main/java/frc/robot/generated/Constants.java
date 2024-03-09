77baf54f4013bf11c122956aed22afc4d7575035
Tsundere808 <xhawaii808@gmail.com>
2024-03-08 18:39:15 -1000

    Initial

---
+++ src/main/java/frc/robot/generated/Constants.java
@@ -0,0 +1,50 @@
+package frc.robot.generated;
+
+public class Constants {
+ 
+        public static final class IntakeConstants {
+        
+            public static final int intakeMotor = 1;
+            public static final int IntakeSPEED = 80;
+
+        }
+
+        
+        public static final class ShooterConstants {
+        
+            public static final int shooterTOP = 4;
+            public static final int shooterBOTTOM = 3;
+            public static final int SHOOTSPEED = 80;
+
+        }
+
+            
+        public static final class FeederConstants {
+        
+            public static final int feeder = 2;
+            public static final int feederEncoderA = 0;
+            public static final int feederEncoderB = 1;
+            public static final int SHOOTSPEED = 80;
+            public static final int IntakeSPEED = 80;
+
+        }
+
+
+        public static final class ElevatorConstants {
+        
+            public static final int leftElevator = 8;
+            public static final int rightElevator = 9;
+            public static final double HOMEPosition = 0;
+            public static final double SetUpPosition = 100;
+            public static final double ClimbedPosition = 25;
+
+        }
+
+        public static final class PivotConstants {
+        
+            public static final int pivot = 31;
+            public static final int pivotspeed = 80;
+        }
+        
+
+}
